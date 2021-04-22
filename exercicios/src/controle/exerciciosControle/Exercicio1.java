2a605972a8867a4fcc768a202445068148777670
Cesar Vicente <cesar_vicente@outlook.com.br>
2021-04-21 22:52:43 -0300

    update code

--- exercicios/src/controle/exerciciosControle/Exercicio1.java
+++ exercicios/src/controle/exerciciosControle/Exercicio1.java
@@ -1,11 +1,31 @@
 package controle.exerciciosControle;
 
+import java.util.Scanner;
+
 public class Exercicio1 {
 /*	1. 	Criar um programa que receba um número e verifique
 	 	se ele está entre 0 e 10 e é par;				*/
 	public static void main(String[] args) {
 		
+		Scanner entrada = new Scanner(System.in);
+		int numero = 0;
 		
+		System.out.println("Para sair digite -100");
+		for (; numero != -100;) {
+			System.out.printf("Insira um número: ");
+			numero = entrada.nextInt();
+			if (numero == -100) break;
+			
+			
+			if (numero >= 0 && numero <= 10) System.out.printf("%d Está entre 0 e 10\n", numero);
+			else System.out.printf("%d não está entre 0 e 10\n", numero);
+			
+			if (numero % 2 == 1) System.out.printf("e não é par");
+			else System.out.println("e é par");
+			System.out.println();
+		}
+		System.out.println("Programa Encerrado!");
+		entrada.close(); 
 		
 	}
 }
--- exercicios/src/controle/exerciciosControle/Exercicio2.java
+++ exercicios/src/controle/exerciciosControle/Exercicio2.java
@@ -1,10 +1,27 @@
 package controle.exerciciosControle;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
 /*2. Criar um programa informa se o ano atual é um ano bissexto;*/
 	public static void main(String[] args) {
 		
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Insira 0 para sair...");
+		int ano = 1;
 		
+		while (ano != 0) {
+			
+			System.out.printf("Digite o Ano: ");
+			ano = entrada.nextInt();
+			if (ano == 0) break;
+			if (ano % 4 == 0) System.out.printf("%d é um ano bissexto.", ano);
+			else System.out.printf("%d não é um ano bissexto.", ano);	
+			System.out.println("\n");
+		}
+		System.out.println("\n\nPrograma Encerrado!");
+		
+		entrada.close();
 		
 	}
 }
