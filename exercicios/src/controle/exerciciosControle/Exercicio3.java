7375914abcb7fa3356dc20bd8f76d9c0a64c4117
Cesar Vicente <cesar_vicente@outlook.com.br>
2021-04-21 23:38:16 -0300

    update

--- exercicios/src/controle/exerciciosControle/Exercicio3.java
+++ exercicios/src/controle/exerciciosControle/Exercicio3.java
@@ -1,5 +1,7 @@
 package controle.exerciciosControle;
 
+import java.util.Scanner;
+
 public class Exercicio3 {
 /*	3. 	Criar um programa que receba duas notas parciais,
   		calcular a média final. Se a nota do aluno for 
@@ -10,7 +12,26 @@ public class Exercicio3 {
 	
 	public static void main(String[] args) {
 		
+		Scanner entrada = new Scanner(System.in);
+		int nota1 = 0;
+		int nota2 = 0;
 		
+		System.out.printf("Digite a 1º nota: ");
+		nota1 = entrada.nextDouble();
+		System.out.printf("Digite a 2º nota: ");
+		nota2 = entrada.nextDouble();
+		
+		double media = (nota1 + nota2) / 2;
+		System.out.println("Media = "+media);
+		if (media >= 7) System.out.println("Aprovado!");
+		else if (media > 4) System.out.println("Recuperação");
+		else System.out.println("Reprovado!");
+		
+		
+		
+		
+		
+		entrada.close();
 		
 	}
 }
