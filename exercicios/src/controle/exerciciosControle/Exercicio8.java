4f0b707ebe0d3404528a0798e5f8e2ff134da27b
Cesar Vicente <cesar_vicente@outlook.com.br>
2021-04-25 14:09:59 -0300

    update

--- exercicios/src/controle/exerciciosControle/Exercicio8.java
+++ exercicios/src/controle/exerciciosControle/Exercicio8.java
@@ -1,10 +1,20 @@
 package controle.exerciciosControle;
 
+import java.util.Scanner;
+
 public class Exercicio8 {
 /*8. Criar um programa que receba uma palavra e imprime no console letra por letra.*/
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.printf("Informe a Palavra: ");
+		String palavra = entrada.nextLine();
 		
 		
+		for (int i = 0; i < palavra.length(); i++) {
+			System.out.println(palavra.charAt(i));
+		}
+		
+		entrada.close();
 		
 	}
 }
