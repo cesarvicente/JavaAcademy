0ca1f05f3e646f3a186aad6b93f4ce3dbe10e77f
Cesar Vicente <cesar_vicente@outlook.com.br>
2021-04-25 14:20:13 -0300

    Finish Tasks 4:

--- exercicios/src/controle/exerciciosControle/Exercicio9.java
+++ exercicios/src/controle/exerciciosControle/Exercicio9.java
@@ -1,10 +1,24 @@
 package controle.exerciciosControle;
 
+import java.util.Scanner;
+
 public class Exercicio9 {
 	/*9. Crie um programa que recebe 10 valores e ao final imprima o maior número.*/
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
 		
+		int valor = 0;
+		int maiorValor = 0;
 		
+		for (int i = 1; i <= 10; i++) {
+			System.out.printf("Digite o valor %d: ", i);
+			valor = entrada.nextInt();
+			
+			if (valor > maiorValor) maiorValor = valor;	
+		}
+		System.out.println("O Maior Valor Digitado foi: " + maiorValor);
+		entrada.close();
+		System.out.println("Programa Finalizado!");
 		
 	}
 }
